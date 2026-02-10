d9ae56caed641b34f6cb454c0582a50a07501582
Anastasiia <skubakovaa@gmail.com>
2026-02-10 17:05:09 +0100

    Add nested ticket creation in OrderSerializer and OrderListSerializer

--- railway/serializers.py
+++ railway/serializers.py
@@ -1,3 +1,4 @@
+from django.db import transaction
 from rest_framework import serializers
 from rest_framework.exceptions import ValidationError
 
@@ -158,3 +159,23 @@ class TicketListSerializer(TicketSerializer):
     class Meta:
         model = Ticket
         fields = ("id", "cargo", "seat", "journey")
+
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    tickets = TicketSerializer(many=True, read_only=False, allow_empty=False)
+
+    class Meta:
+        model = Order
+        fields = ("id", "tickets", "created_at")
+
+    def create(self, validated_data):
+        with transaction.atomic():
+            tickets_data = validated_data.pop("tickets")
+            order = Order.objects.create(**validated_data)
+            for ticket_data in tickets_data:
+                Ticket.objects.create(order=order, **ticket_data)
+            return order
+
+class OrderListSerializer(OrderSerializer):
+    tickets = TicketListSerializer(many=True, read_only=True)
