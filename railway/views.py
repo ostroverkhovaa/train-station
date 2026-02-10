67cc6235e88a6a5c554ae7544bbc01f2b5d48aa0
Anastasiia <skubakovaa@gmail.com>
2026-02-10 18:22:52 +0100

    add TrainViewSet

--- railway/views.py
+++ railway/views.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
-from railway.models import Station, Route, TrainType
-from railway.serializers import StationSerializer, RouteSerializer, RouteListSerializer, RouteDetailSerializer, \
-    TrainTypeSerializer
+from railway.models import Station, Route, TrainType, Train
+from railway.serializers import (
+    StationSerializer,
+    RouteSerializer,
+    RouteListSerializer,
+    RouteDetailSerializer,
+    TrainTypeSerializer,
+    TrainListSerializer,
+    TrainDetailSerializer,
+    TrainSerializer,
+)
 
 
 class StationViewSet(viewsets.ModelViewSet):
@@ -30,4 +38,13 @@ class TrainTypeViewSet(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.select_related("train_type")
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
+    def get_serializer_class(self):
+        if self.action == "list":
+            return TrainListSerializer
+        if self.action == "retrieve":
+            return TrainDetailSerializer
+        return TrainSerializer
